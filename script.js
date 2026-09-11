94e13e2ff397f4169de09e8374f9d1a73047d7a2
AndrePazini <pazinihoch@gmail.com>
2026-09-11 11:02:10 -0300

    feat: adiciona menu hamburguer em telas menores

--- script.js
+++ script.js
@@ -16,3 +16,27 @@ function typewriter(elementId, texto, velocidade = 80) {
 
 // Uso:
 typewriter("typewriter-text", "Desenvolvedor FullStack em Formação", 80);
+
+
+
+const btnMenu = document.getElementById('btn-menu');
+const menuNavegacao = document.querySelector('.menu-navegacao');
+const overlayMenu = document.getElementById('overlay-menu');
+
+function toggleMenu() {
+    btnMenu.classList.toggle('ativo');
+    menuNavegacao.classList.toggle('ativo');
+    overlayMenu.classList.toggle('ativo');
+}
+
+btnMenu.addEventListener('click', toggleMenu);
+overlayMenu.addEventListener('click', toggleMenu);
+
+// Fecha o menu automaticamente ao clicar em um link
+document.querySelectorAll('.menu-navegacao a').forEach(link => {
+    link.addEventListener('click', () => {
+        if (menuNavegacao.classList.contains('ativo')) {
+            toggleMenu();
+        }
+    });
+});
